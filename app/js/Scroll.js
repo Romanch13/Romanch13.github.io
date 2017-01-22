95f3191b36dd4f1894b6be7e3e5b274d04ed2e9c
Romanch13 <zakyska1997@gmail.com>
2017-01-23 01:58:00 +0200

    diplomna 21/01/17 01:57
    diplomna 21/01/17 01:57

---
+++ app/js/Scroll.js
@@ -0,0 +1,51 @@
+//ScrollSectionPageId
+$(document).ready(function(){
+		$('nav a[href^="#"]').click(function () { 
+ 				elementClick = $(this).attr("href");
+				destination = $(elementClick).offset().top - 50;
+				{
+				$('body,html').animate( { scrollTop: destination }, 1500);
+				} 
+				return false;
+		   });
+});
+//scrollToTop
+$(function() {
+		$(window).scroll(function() {
+			if($(this).scrollTop() > 600) {
+			$('#toTop').fadeIn();
+				} else {
+			$('#toTop').fadeOut();
+				}
+			});
+			$('#toTop').click(function() {
+			$('body,html').animate({scrollTop:0},1000);
+			return false;
+		});
+	});
+//Меню jquery
+$(document).ready(function(){
+			var touch = $('#touch-menu');
+		    var menu = $('.nav');
+		 
+		    $(touch).on('click', function(e){
+		        e.preventDefault();
+		        menu.slideToggle();
+		    });
+		    $(window).resize(function(){
+		        var wid = $(window).width();
+		        if(wid > 768 && menu.is(':hidden')){
+		            menu.removeAttr('style');
+		        }
+		    });
+		
+		});
+//button carousel scroll
+	$(document).ready(function() {
+   
+    $('#about-us').click(function(){
+        $('html, body').animate({scrollTop:$(document).height(500)}, 'slow');
+        return false;
+    });
+
+});
